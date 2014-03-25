ca14d6aea84028ee69ae2a38742df186d3bc7f4c
jsAaron <401545553@qq.com>
2014-03-25 17:27:36 +0800

    up

--- src/demo.js
+++ src/demo.js
@@ -2,6 +2,8 @@
 
 var a = avalon.define("box", function(vm) {
     vm.w = 100;
+    vm.y = 'hahahh'
+    vm.z = '啊是大师傅'
 //        vm.h = 100;
     vm.click = function() {
         vm.w = parseFloat(vm.w) + 10;
